716b2d8b86d6fbbc1ec1535254a4a6c53933c350
unknown <hosseinnematicg@gmail.com>
2023-10-19 12:52:19 +0330

    homepage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as X } from "react-router-dom";
 import App from "./a01.js";
 //import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={"/"}>
+  <X>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </X>
 );
