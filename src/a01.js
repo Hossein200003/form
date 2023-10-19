ca8eaae5c015ed3cc9f03218c007d82943d86835
unknown <hosseinnematicg@gmail.com>
2023-10-19 12:03:09 +0330

    A

--- src/a01.js
+++ src/a01.js
@@ -7,7 +7,7 @@ const RouteApp = () => {
     <>
       <div className="mainDiv">
         <Routes>
-          <Route path="/" element={<P1 />} />
+          <Route path="/x" element={<P1 />} />
           <Route path="/page2" element={<P2 />} />
           <Route path="/page3" element={<P3 />} />
           <Route path="/page4" element={<P4 />} />
@@ -61,11 +61,11 @@ const P1 = () => {
           <p>Please provide your name, email address and phone number.</p>
           <div className="input">
             <label>Name:</label>
-            <input type="text" placeholder="Your Name"/>
+            <input type="text" placeholder="Your Name" />
             <label>Email Address:</label>
-            <input type="text" placeholder="Email Address"/>
+            <input type="text" placeholder="Email Address" />
             <label>Phone Number:</label>
-            <input type="text" placeholder="Phone Number"/>
+            <input type="text" placeholder="Phone Number" />
           </div>
           <div className="btn">
             <div>Go Back</div>
