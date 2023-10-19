3eb788edd7133bacc4647b9f7c23c7512f32d8e8
unknown <hosseinnematicg@gmail.com>
2023-10-19 11:35:22 +0330

    basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./a01.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename={"/"}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
