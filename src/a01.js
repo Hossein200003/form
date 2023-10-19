80cb24dcce7f672d83bfa53d1c20e57aa6414a0b
unknown <hosseinnematicg@gmail.com>
2023-10-19 13:00:06 +0330

    hi removed

--- src/a01.js
+++ src/a01.js
@@ -6,7 +6,6 @@ const RouteApp = () => {
   return (
     <>
       <div className="mainDiv">
-        <h1>Hi</h1>
         <Routes>
           <Route path="/" element={<P1 />} />
           <Route path="/page2" element={<P2 />} />
